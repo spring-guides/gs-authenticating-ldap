1b05120bf1c6c0c636871ae480aedf1730556166
Greg Turnquist <gturnquist@vmware.com>
2013-04-26 16:59:51 -0500

    Implemented start.
    Need to go back and edit the guide to match where the start and complete points are.

---
+++ start/src/main/java/ldapauthentication/HomeController.java
@@ -0,0 +1,14 @@
+package ldapauthentication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public @ResponseBody String index() {
+		return "Welcome to the home page!";
+	}
+}
---
+++ start/src/main/java/ldapauthentication/RootConfig.java
@@ -0,0 +1,9 @@
+package ldapauthentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+public class RootConfig {
+	
+}
