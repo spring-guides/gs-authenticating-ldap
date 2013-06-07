e210a196eacc9a9b48a806b1743747b5d202c0ed
Greg Turnquist <gturnquist@gopivotal.com>
2013-06-07 13:29:21 -0400

    Overhaul guide to reflect current guidelines
    Rewrote guide from ground up to reflect current guide line standards.
    Also replaced gradle with maven. Using bootstrap allowed me to cut
    out probably 3/4 of the code, making it much simpler to demonstrate
    Spring Security LDAP. Still need to investigate the Javaconfig
    API Rob Winch recently released.

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.bootstrap.SpringApplication;
+import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
