bd017860a40953c26d615d764277bc054c5c1edc
Dave Syer <dsyer@pivotal.io>
2014-08-21 17:09:34 +0100

    Alternative workaround for NPE in Boot 1.1.5
    This change works around some early instantiation issues in
    Spring Boot 1.1.5 with authentication builders. The original
    attempt at the workaround borrowed the wrong code from
    gs-securiing-web

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -3,32 +3,29 @@ package hello;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder
-                .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
-                .contextSource()
-                .ldif("classpath:test-server.ldif");
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
+				.fullyAuthenticated().and().formLogin();
+	}
 
-    @Configuration
-    protected static class AuthenticationConfiguration extends
-            GlobalAuthenticationConfigurerAdapter {
+	@Configuration
+	protected static class AuthenticationConfiguration extends
+			GlobalAuthenticationConfigurerAdapter {
 
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth
-                    .inMemoryAuthentication()
-                    .withUser("user").password("password").roles("ROLE_USER");
-        }
-    }
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
+					.groupSearchBase("ou=groups").contextSource()
+					.ldif("classpath:test-server.ldif");
+		}
+	}
 }
