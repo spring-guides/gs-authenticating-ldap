5d5348f01374712d629d1d509983630f2745d84c
Greg Turnquist <gturnquist@gopivotal.com>
2015-03-05 13:55:29 -0600

    Handle difference between maven and gradle
    Gradle can't resolve spring-tx properly, so it must be explicitly declared. See http://www.tagwith.com/question_74634_spring-ldap-example-requires-persistence
    
    Resolves #8, #9

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -13,8 +13,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
-				.fullyAuthenticated().and().formLogin();
+		http
+			.authorizeRequests()
+				.antMatchers("/css/**").permitAll()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
 	}
 
 	@Configuration
@@ -23,9 +27,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Override
 		public void init(AuthenticationManagerBuilder auth) throws Exception {
-			auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
-					.groupSearchBase("ou=groups").contextSource()
-					.ldif("classpath:test-server.ldif");
+			auth
+				.ldapAuthentication()
+					.userDnPatterns("uid={0},ou=people")
+					.groupSearchBase("ou=groups")
+					.contextSource().ldif("classpath:test-server.ldif");
 		}
 	}
 }
