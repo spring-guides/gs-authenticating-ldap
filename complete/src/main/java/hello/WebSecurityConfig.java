e2bee44edfbaa38434b592e45f12c3f70646f046
Greg Turnquist <gturnquist@pivotal.io>
2016-03-11 12:13:17 -0600

    Cleanup guide

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -4,18 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/css/**").permitAll()
 				.anyRequest().fullyAuthenticated()
 				.and()
 			.formLogin();
