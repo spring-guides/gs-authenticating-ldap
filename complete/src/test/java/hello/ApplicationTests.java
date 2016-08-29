0d0789e50aa824b133f5ee363794ef6023fd8b38
Rob Winch <rwinch@gopivotal.com>
2016-08-29 11:25:08 -0500

    Add MockMvc Tests

---
+++ complete/src/test/java/hello/ApplicationTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hello;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * 
+ * @author Rob Winch
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void loginWithValidUserThenAuthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+            .user("ben")
+            .password("benspassword");
+
+        mockMvc.perform(login)
+            .andExpect(authenticated().withUsername("ben"));
+    }
+
+    @Test
+    public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+            .user("invalid")
+            .password("invalidpassword");
+
+        mockMvc.perform(login)
+            .andExpect(unauthenticated());
+    }
+}
