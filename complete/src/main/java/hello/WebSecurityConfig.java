95a4e9036a81201f554937dcc36ea2b12bec35c7
amseager <amseager@users.noreply.github.com>
2018-02-13 00:10:50 +0300

    add missing @EnableWebSecurity annotation
    Connected issue: https://github.com/spring-guides/gs-authenticating-ldap/issues/21

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
