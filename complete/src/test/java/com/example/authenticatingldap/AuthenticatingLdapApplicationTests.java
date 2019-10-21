65b11d60e61e4f5992292d9ad3c6999cb660bd19
Jay Bryant <jbryant@pivotal.io>
2019-10-21 15:48:33 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2, which necessitated upgrading JUnit to version 5 and replacing the Gradle Wrappers.

--- complete/src/test/java/com/example/authenticatingldap/AuthenticatingLdapApplicationTests.java
+++ complete/src/test/java/com/example/authenticatingldap/AuthenticatingLdapApplicationTests.java
@@ -18,20 +18,17 @@ package com.example.authenticatingldap;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
  *
  * @author Rob Winch
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTests {
