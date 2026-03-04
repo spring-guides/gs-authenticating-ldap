d550ce9fa5bda0239fb518088e2ae3cea2aa9357
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-03-04 09:40:08 -0700

    Update to Boot 4
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- complete/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
+++ complete/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -1,17 +1,49 @@
 package com.example.authenticatingldap;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+	@Bean
+	public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
+		EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
+				EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
+		contextSourceFactoryBean.setPort(8389);
+		return contextSourceFactoryBean;
+	}
+
+	@Bean
+	public LdapAuthoritiesPopulator authorities(BaseLdapPathContextSource contextSource) {
+		DefaultLdapAuthoritiesPopulator authorities =
+				new DefaultLdapAuthoritiesPopulator(contextSource, "ou=groups");
+		authorities.setGroupSearchFilter("uniqueMember={0}");
+		return authorities;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+			BaseLdapPathContextSource contextSource,
+			LdapAuthoritiesPopulator authorities) {
+		LdapPasswordComparisonAuthenticationManagerFactory factory =
+				new LdapPasswordComparisonAuthenticationManagerFactory(
+						contextSource, new BCryptPasswordEncoder());
+		factory.setUserDnPatterns("uid={0},ou=people");
+		factory.setPasswordAttribute("userPassword");
+		factory.setLdapAuthoritiesPopulator(authorities);
+		return factory.createAuthenticationManager();
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,32 +56,4 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.ldapAuthentication()
-				.userDnPatterns("uid={0},ou=people")
-				.groupSearchBase("ou=groups")
-				.contextSource()
-					.url("ldap://localhost:8389/dc=springframework,dc=org")
-					.and()
-				.passwordCompare()
-					.passwordEncoder(new BCryptPasswordEncoder())
-					.passwordAttribute("userPassword");
-	}
-
 }
-
-//@Configuration
-//public class WebSecurityConfig {
-
-//	@Bean
-//	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//		return http
-//			.authorizeRequests()
-//			.anyRequest().authenticated()
-//			.and()
-//			.formLogin(Customizer.withDefaults())
-//			.build();
-//	}
-//}
