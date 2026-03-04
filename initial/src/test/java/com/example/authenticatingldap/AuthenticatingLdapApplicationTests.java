7de9fc9257898448208520d14a79502b58a58c4c
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-03-03 17:31:07 -0700

    Polish README an d initial
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

---
+++ initial/src/test/java/com/example/authenticatingldap/AuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
